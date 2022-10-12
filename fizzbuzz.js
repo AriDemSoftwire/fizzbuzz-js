f26f1e5ecf354f5338a55c6c8d69162817b95587
AriDemSoftwire <arina.demidova@softwire.com>
2022-10-12 14:54:45 +0100

    Tasks complete - I hope?

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,38 +1,38 @@
 // Here, we create our main function.
 function fizzbuzz() {
+  let fizz = "Fizz";
+  let buzz = "Buzz";
+  let bang = "Bang";
+  let bong = "Bong";
+  let fezz = "Fezz";
 
-  let fizz = 'Fizz';
-  let buzz = 'Buzz';
-  let bang = 'Bang';
-  let bong = 'Bong'; 
-  let fezz = 'Fezz';
-  
-    for (let i = 1; i <= 300; i++) {
-        let wordToLog = [];
-        let wordToLogReverse = []; 
-        if (i % 3 === 0) {
-          wordToLog.push(fizz);
-        }
-        if (i % 13 === 0) {
-          wordToLog.push(bong);
-        }
-        if (i % 5 === 0) {
-          wordToLog.push(buzz);
-        }
-        if (i % 7 === 0) {
-          wordToLog.push(bang);
-        }
-        if (i % 17 === 0 && i > 34) {
-     wordToLogReverse = wordToLog.reverse();
-     console.log(wordToLogReverse.join()); 
-        } else if (i % 3 === 0 || i % 5 === 0 || i % 7 === 0) {
-          console.log(wordToLog.join());
-        } else if (i % 11 === 0) {
-            console.log(bong);
-        } else {
-          console.log(i);
-        }
-      } 
+  for (let i = 1; i <= 2500; i++) {
+    let wordToLog = [];
+    let wordToLogReverse = [];
+    if (i % 3 === 0 && i % 11 !== 0) {
+      wordToLog.push(fizz);
+    }
+    if (i % 13 === 0) {
+      wordToLog.push(fezz);
+    }
+    if (i % 5 === 0 && i % 11 !== 0) {
+      wordToLog.push(buzz);
+    }
+    if (i % 7 === 0 && i % 11 !== 0) {
+      wordToLog.push(bang);
+    }
+    if (i % 11 === 0) {
+      wordToLog.push(bong);
+    }
+    if (i % 17 === 0 && wordToLog.length > 0) {
+      wordToLogReverse = wordToLog.reverse();
+      console.log(wordToLogReverse.join(""));
+    } else if (i % 3 === 0 || i % 5 === 0 || i % 7 === 0 || i % 13 === 0 || i % 11 === 0) {
+      console.log(wordToLog.join(""));
+    } else {
+      console.log(i);
+    }
+  }
 }
 
 // Now we run the main function...
