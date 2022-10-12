3764a3caf8a9a90f78ddb6cd6b3b687f1d5dc817
AriDemSoftwire <arina.demidova@softwire.com>
2022-10-12 12:39:59 +0100

    FizzBuzz with variable

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -3,11 +3,23 @@
 // Here, we create our main function.
 function fizzbuzz() {
     
-    console.log('Welcome to FizzBuzz!');
-
-    // Put your code here...
-
-    
+    for (let i = 1; i <= 110; i++) {
+        let wordToLog = "";
+        if (i % 3 === 0) {
+          wordToLog = wordToLog + "Fizz";
+        }
+        if (i % 5 === 0) {
+          wordToLog = wordToLog + "Buzz";
+        }
+        if (i % 7 === 0) {
+          wordToLog = wordToLog + "Bang";
+        }
+        if (i % 3 === 0 || i % 5 === 0 || i % 7 === 0) {
+          console.log(wordToLog);
+        } else {
+          console.log(i);
+        }
+      } 
 }
 
 // Now we run the main function...
