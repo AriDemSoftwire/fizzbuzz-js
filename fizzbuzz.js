3375c25c52cc3108272dad332922b6996aa81d4f
AriDemSoftwire <arina.demidova@softwire.com>
2022-10-12 14:12:14 +0100

    Tasks 1, 2, 4

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,21 +1,34 @@
-
-
 // Here, we create our main function.
 function fizzbuzz() {
-    
-    for (let i = 1; i <= 110; i++) {
-        let wordToLog = "";
+
+  let fizz = 'Fizz';
+  let buzz = 'Buzz';
+  let bang = 'Bang';
+  let bong = 'Bong'; 
+  let fezz = 'Fezz';
+  
+    for (let i = 1; i <= 300; i++) {
+        let wordToLog = [];
+        let wordToLogReverse = []; 
         if (i % 3 === 0) {
-          wordToLog = wordToLog + "Fizz";
+          wordToLog.push(fizz);
+        }
+        if (i % 13 === 0) {
+          wordToLog.push(bong);
         }
         if (i % 5 === 0) {
-          wordToLog = wordToLog + "Buzz";
+          wordToLog.push(buzz);
         }
         if (i % 7 === 0) {
-          wordToLog = wordToLog + "Bang";
+          wordToLog.push(bang);
         }
-        if (i % 3 === 0 || i % 5 === 0 || i % 7 === 0) {
-          console.log(wordToLog);
+        if (i % 17 === 0 && i > 34) {
+     wordToLogReverse = wordToLog.reverse();
+     console.log(wordToLogReverse.join()); 
+        } else if (i % 3 === 0 || i % 5 === 0 || i % 7 === 0) {
+          console.log(wordToLog.join());
+        } else if (i % 11 === 0) {
+            console.log(bong);
         } else {
           console.log(i);
         }
